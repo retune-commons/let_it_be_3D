282aaac5ea966ffdc6b433379a77bbfd49d5d718
Konstantin <kobel.konstantin@web.de>
2023-02-10 11:43:40 +0100

    code review tests

--- core/user_specific_rules.py
+++ core/user_specific_rules.py
@@ -3,13 +3,12 @@ from .utils import convert_to_path
 def user_specific_rules_on_videometadata(videometadata: "VideoMetadata"):
     if videometadata.filepath.suffix == '.AVI':
         videometadata.cam_id = "Top"
-        try:
-            new_filepath = convert_to_path(videometadata.filepath.name.replace(
+        if "00" in videometadata.filepath.name:
+            new_filepath = videometadata.filepath.with_name(videometadata.filepath.name.replace(
                 videometadata.filepath.name[
                     videometadata.filepath.name.index("00") : videometadata.filepath.name.index("00") + 3
                 ],
                 "",
             ))
             videometadata.filepath.rename(new_filepath)
-        except:
-            pass
+            videometadata.filepath = new_filepath
