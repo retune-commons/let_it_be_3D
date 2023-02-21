edeb86305144de0ddfbdebb9ec411ba1beeeeede
MSchellenberger <michael.sbg@gmx.de>
2023-02-21 18:56:40 +0100

    Added code for checking number of frames of the videos of a recording day

---
+++ core/filenumber_checker.py
@@ -0,0 +1,40 @@
+from typing import List
+from pathlib import Path
+
+import pandas as pd
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+from utils import check_if_same, convert_to_path
+
+class Framenumberchecker():
+    """Checks if framenumbers of all videos are the same
+    Input: List of videofilepaths
+    """
+    def check_framenumber(self, videofilepaths: List) -> bool:
+        """checks if all videos in videofilepaths list have the same number of frames"""
+        video_framenumbers = []
+        for elem in videofilepaths:
+            video = Framenumberreader()
+            framenumber = video.run(convert_to_path(elem))
+            video_framenumbers.append(framenumber)
+        if check_if_same(video_framenumbers):
+            print('All videos have the same amount of frames!')
+        else:
+            print('Not all videos have the same number of frames!')
+            pd.DataFrame(video_framenumbers, index = [videofilepaths])
+
+
+class Framenumberreader():
+
+    """
+    Reads a video, checks & returns the number of frames
+    """
+
+    def run(self, videofilepath: Path) -> int:
+        return(self.read_framenr(self.read_video(videofilepath)))
+
+    def read_video(self, videofilepath: Path) -> None:
+        return VideoFileClip(videofilepath)
+
+    def read_framenr(self, video) -> int:
+        return video.reader.nframes
